3a6b4ea5bd2bd4d4d74d7bcbe657b43e4b1ce94b
noynathyk <noynathykh007@gmail.com>
2026-05-29 00:01:54 +0700

    Updates

--- assets/IntroSections-BuNZ69xh.js
+++ assets/IntroSections-BuNZ69xh.js
@@ -1,4 +1,4 @@
-import{c as n,r,j as e,m as i}from"./index-CBi_Dxpt.js";/**
+import{c as n,r,j as e,m as i}from"./index-7BkwApc0.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/ParentsSection-Bw4u3MW8.js
+++ assets/ParentsSection-Bw4u3MW8.js
@@ -1,4 +1,4 @@
-import{c as t,r,j as e}from"./index-CBi_Dxpt.js";/**
+import{c as t,r,j as e}from"./index-7BkwApc0.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
